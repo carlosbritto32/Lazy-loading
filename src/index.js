dc08dbac83ae3dbcdd1d896f01f2e90d70faba82
Carlos Britto <carlosbritto32@gmail.com>
2022-01-13 12:17:06 -0500

    Applying lazy loading for project using Intersection Observer API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// url para practica baseUrl2=https://source.unsplash.com/random
 
-console.log('Happy hacking :)')
+import { registerImage } from "./lazy";
+
+const max = 122;
+const min = 1;
+const random = () => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const createImageNode = () => {
+  const container = document.createElement("div");
+  container.className = "p-4";
+
+  const image = document.createElement("img");
+  image.className = "mx-auto";
+  image.width = "320";
+  image.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+  container.appendChild(image);
+  return container;
+};
+
+const mountNode = document.getElementById("images");
+
+const addButton = document.querySelector("button");
+
+const addImage = () => {
+  const newImage = createImageNode();
+  mountNode.appendChild(newImage);
+  registerImage(newImage);
+};
+
+addButton.addEventListener("click", addImage);

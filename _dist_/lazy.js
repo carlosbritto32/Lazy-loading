2f6b6161e7c07e705c389072b0c8e1f9beb49b94
carlosbritto32 <carlosbritto32@users.noreply.github.com>
2022-01-13 17:17:54 +0000

    deploy: dc08dbac83ae3dbcdd1d896f01f2e90d70faba82

---
+++ _dist_/lazy.js
@@ -0,0 +1,23 @@
+const isIntersecting = (entry) => {
+  return entry.isIntersecting;
+};
+
+const loadImage = (entry) => {
+  const container = entry.target;
+  const image = container.querySelector("img");
+  const url = image.dataset.src;
+
+  image.src = url;
+
+  // container.dataset.src;
+
+  observer.unobserve(container);
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.filter(isIntersecting).forEach(loadImage);
+});
+
+export const registerImage = (image) => {
+  observer.observe(image);
+};
